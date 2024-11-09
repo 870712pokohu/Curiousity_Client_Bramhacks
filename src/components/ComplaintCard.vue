25de304bd219671edc9e2a3887fb4750f78ee1d1
870712pokohu <870712pokohu@gmail.com>
2024-11-09 16:56:51 -0500

    update changes for admin page

---
+++ src/components/ComplaintCard.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card class="rounded-lg d-flex algin-center justify-center" style="min-width: 300px; min-height: 300px">
+    <div class="d-flex flex-column flex-wrap align-stretch justify-center pa-5">
+      <div class="text-h5">{{complaint.title}}</div>
+      <div class="mt-2 text-subtitle-1">{{complaint.description}}</div>
+    </div>
+  </v-card>
+</template>
+
+<script setup>
+
+const props = defineProps({
+  complaint: Object,
+});
+
+</script>
---
+++ src/components/Dailogs/ComplaintCardDetailDialog.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card class="pa-5" style="min-height: 700px;">
+    <div>{{complaint.title}}</div>
+  </v-card>
+</template>
+
+
+<script setup>
+
+const props = defineProps({
+  complaint: Object,
+});
+
+console.log('complaint', props.complaint);
+
+</script>
